f7e053048758f9358ef0815861bb23034922adab
Md Rabbi Hossain <rabbi.hossain477@gmail.com>
2020-11-01 20:18:38 -0800

    fifth commit

---
+++ LAB_TASK_3.1_PHP/TASK_2_CURRENT_PAGE/degreeCheck1.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+if(empty($_POST['degree'])){
+    
+    echo "Nothing was selected";
+    
+}
+else{
+    
+    $degree = $_POST['degree'];
+    
+    for($i = 0; $i < count($degree); $i++){
+        
+        echo "Degree: ".$degree[$i]."<br>";
+        
+    }
+    
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>HTML Form - Degree</title>
+</head>
+
+<body>
+
+    <form method="post" action="degreeCheck1.php">
+
+        <fieldset style="width:250px">
+
+            <legend><b>DEGREE</b></legend>
+            <input type="checkbox" name="degree[]" value="SSC"> SSC
+            <input type="checkbox" name="degree[]" value="HSC"> HSC
+            <input type="checkbox" name="degree[]" value="Bsc"> Bsc
+            <input type="checkbox" name="degree[]" value="Msc"> Msc
+            <hr>
+            <input type="submit" value="Submit">
+        </fieldset>
+
+
+
+
+    </form>
+
+</body>
+
+</html>
