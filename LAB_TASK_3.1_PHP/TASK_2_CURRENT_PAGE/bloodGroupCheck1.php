793c33a3551a09ecb8341232e6a716a9d84bd97c
Md Rabbi Hossain <rabbi.hossain477@gmail.com>
2020-11-01 20:30:56 -0800

    seventh commit

---
+++ LAB_TASK_3.1_PHP/TASK_2_CURRENT_PAGE/bloodGroupCheck1.php
@@ -0,0 +1,39 @@
+<?php
+
+echo "Blood Group: ".$_POST['bloodGroup'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HTML Form - Blood Group</title>
+</head>
+<body>
+    
+    <form method="post" action="bloodGroupCheck1.php">
+        
+        <fieldset style="width:200px">
+            
+            <legend><b>BLOOD GROUP</b></legend>
+            <select name="bloodGroup">
+				<option value="A+">A+</option>
+				<option value="A-">A-</option>
+				<option value="B+">B+</option>
+				<option value="B-">B-</option>
+				<option value="AB+">AB+</option>
+				<option value="AB-">AB-</option>
+				<option value="O+"  selected >O+</option>
+				<option value="O-">O-</option>
+			</select>
+			<hr>
+   
+       <input type="submit" value="Submit">
+        </fieldset>
+        <br>
+        
+        
+        
+    </form>
+    
+</body>
+</html>
