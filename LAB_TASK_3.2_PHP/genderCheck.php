ddc8e6c88c83da4c51769e169ad2e63262f22875
Md Rabbi Hossain <rabbi.hossain477@gmail.com>
2020-11-04 08:27:07 -0800

    third commit

---
+++ LAB_TASK_3.2_PHP/genderCheck.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_REQUEST['submit'])){
+    
+    if(isset($_REQUEST['gender'])){
+        
+        echo "Gender: ".$_REQUEST['gender'];
+        
+    }
+    
+    else{
+        
+        header('location: gender.html?msg=null_gender');
+        
+    }
+    
+    
+    
+}
+
+else{header('location: gender.html?msg=null_gender');}
+
+
+?>
