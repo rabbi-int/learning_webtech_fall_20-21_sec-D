4a5977e659e4bfe9c4519939f5869a484dcbc2db
Md Rabbi Hossain <rabbi.hossain477@gmail.com>
2020-12-01 19:05:18 -0800

    second commit

--- FINAL_LAB_TASK_1.2_PHP/controller/registrationCheck.php
+++ FINAL_LAB_TASK_1.2_PHP/controller/registrationCheck.php
@@ -1,10 +1,22 @@
 <?php
 
+require_once('../models/userService.php');
+
 if(isset($_REQUEST['submit'])){
     
     if(!empty($_REQUEST['name']) and !empty($_REQUEST['contactNo']) and !empty($_REQUEST['username']) and !empty($_REQUEST['password'])){
         
+        if(userRegistration($_REQUEST['name'], $_REQUEST['contactNo'], $_REQUEST['username'], $_REQUEST['password'])){
+            
+            header('location: ../view/registration.php?msg=registration_completed');
+            
+        }
         
+        else{
+            
+            header('location: ../view/registration.php?msg=registration_failed');
+            
+        }
         
     }
     
